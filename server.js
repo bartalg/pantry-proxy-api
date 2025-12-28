682a7d6973562911c6f349f931e0a8437aebfdae
bartalg <bartalg@gmail.com>
2025-12-28 08:14:44 +0300

    final commit

--- server.js
+++ server.js
@@ -17,6 +17,12 @@ const PORT = process.env.PORT || 3000;
 // Assign our PANTRY_ID from .env
 const PANTRY_ID = process.env.PANTRY_ID;
 
+// This is our base URL. This will allow us to use this proxy API for multiple different baskets, therefore, we make it generic to make it reusable
+const PANTRY_API_BASE_URL = `https://getpantry.cloud/apiv1/pantry/${PANTRY_ID}/basket`;
+
+app.use(cors());
+app.use(express.json()); // This will handle our JSON calls
+
 // We serve the HTML page
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
@@ -25,6 +31,52 @@ app.get("/", (req, res) => {
 // We also serve the icon file
 app.get("/favicon.ico", (req, res) => {
     res.sendFile(path.join(__dirname, "favicon.ico"));
+});
+
+// Generic handler for GET request to fect data from specific basket
+app.get("/:basketName", async(req, res) => {
+    const {basketName} = req.params;
+    try {
+        const response = await axios.get(`${PANTRY_API_BASE_URL}/${basketName}`);
+        res.json(response.data); // We return the JSON of our response data
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+});
+
+// Generic handler for POST request to add new data from specific basket
+app.post("/:basketName", async(req, res) => {
+    const {basketName} = req.params;
+    const newData = req.body;  // We add the newData for the new data
+    try {
+        const response = await axios.post(`${PANTRY_API_BASE_URL}/${basketName}`, newData); // Add new data
+        res.json(response.data); // We return the JSON of our response data
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+});
+
+// Generic handler for PUT request to update data in a specific basket
+app.put("/:basketName", async(req, res) => {
+    const {basketName} = req.params;
+    const newData = req.body;  // We add the newData for the new data
+    try {
+        const response = await axios.put(`${PANTRY_API_BASE_URL}/${basketName}`, newData); // Update data
+        res.json(response.data); // We return the JSON of our response data
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+});
+
+// Generic handler for DELETE request to delete a specific basket
+app.delete("/:basketName", async(req, res) => {
+    const {basketName} = req.params;
+    try {
+        const response = await axios.delete(`${PANTRY_API_BASE_URL}/${basketName}`);
+        res.json({ message: `Basket ${basketName} cleared successfully.` }); // We return the JSON of our response data
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
 });
 
 // Create a basic EXPRESS app
